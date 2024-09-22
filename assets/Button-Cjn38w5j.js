d1ae4b630232d035674f8265f63b20788319f99e
codefeels <colindiesh@protonmail.com>
2024-09-22 00:43:08 -0400

    Updates

---
+++ assets/Button-Cjn38w5j.js
@@ -0,0 +1,2 @@
+import{j as s}from"./index-BhbqdorF.js";function x({onClick:r,type:t,disabled:a,children:o,id:e,className:g}){return s.jsx("button",{id:e,className:["bg-gray-300 hover:bg-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 text-gray-800 dark:text-gray-100 p-1 m-1 rounded",g].filter(n=>!!n).join(" "),disabled:a,onClick:r,type:t,children:o})}export{x as B};
+//# sourceMappingURL=Button-Cjn38w5j.js.map
