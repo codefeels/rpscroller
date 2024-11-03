7b4e33e5e24cff3f1bf293cfc5354fd44e067185
codefeels <colindiesh@protonmail.com>
2024-11-03 17:43:31 -0500

    Small refactoring

--- src/HeaderBar.tsx
+++ src/HeaderBar.tsx
@@ -14,38 +14,50 @@ import { GiHamburgerMenu } from 'react-icons/gi'
 // utils
 import { useSmallScreen } from './useSmallScreen'
 
+function Logo() {
+  return (
+    <>
+      <a href="/">rpscroller</a>
+      <img className="h-8 inline" src={flame} alt="app icon" />
+    </>
+  )
+}
+
+function Spacer() {
+  return <div className="grow" />
+}
+
+function HamburgerMenu() {
+  const store = useAppStore()
+  return (
+    <GiHamburgerMenu
+      id="menubutton"
+      className="h-8 w-8 inline hover:bg-gray-300 dark:hover:bg-gray-600 mr-1"
+      onClick={() => {
+        store.toggleSidebarOpen()
+      }}
+    />
+  )
+}
+
 export default function HeaderBar() {
   const store = useAppStore()
   const { headerOnBottomOfScreen } = store
   const small = useSmallScreen()
-  return headerOnBottomOfScreen ? (
-    <div className="lg:p-2 flex myheader z-10 w-full border-t-2 border-slate-500 fixed bottom-0 right-0">
+  return small && headerOnBottomOfScreen ? (
+    <div className="lg:p-2 myheader z-10 w-full border-t-2 border-slate-500 fixed flex bottom-0 right-0">
       <Sorts />
-      <div className="grow" />
-      <GiHamburgerMenu
-        id="menubutton"
-        className="h-8 w-8 inline hover:bg-gray-300 dark:hover:bg-gray-600 mr-1"
-        onClick={() => {
-          store.toggleSidebarOpen()
-        }}
-      />
-      rpscroller
-      <img className="h-8 inline" src={flame} alt="app icon" />
+      <Spacer />
+      <HamburgerMenu />
+      <Logo />
     </div>
   ) : (
     <div className="lg:p-2 myheader z-10 w-full border-b-2 border-slate-500 flex sticky top-0">
-      <GiHamburgerMenu
-        id="menubutton"
-        className="h-8 w-8 inline hover:bg-gray-300 dark:hover:bg-gray-600 mr-1"
-        onClick={() => {
-          store.toggleSidebarOpen()
-        }}
-      />
-      rpscroller
-      <img className="h-8 inline" src={flame} alt="app icon" />
-      <div className="grow" />
+      <HamburgerMenu />
+      <Logo />
+      <Spacer />
       {small ? null : <SearchBox />}
-      <div className="grow" />
+      <Spacer />
       <Sorts />
     </div>
   )
