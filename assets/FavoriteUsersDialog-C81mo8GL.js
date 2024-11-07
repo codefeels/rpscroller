c9a913c620d50841b2b62f0469e242e115f9dd7a
codefeels <colindiesh@protonmail.com>
2024-11-07 16:22:53 -0500

    Updates

--- assets/FavoriteUsersDialog-C81mo8GL.js
+++
@@ -1,2 +0,0 @@
-import{u as i,j as m,k as n}from"./index-lChYP5aH.js";import{F as p}from"./FavoritesDialog-Bbl6t800.js";import"./en-US-Ckk-QizZ.js";function v({open:o,onClose:r}){const t=i(),{favorites:s}=t;return m.jsx(p,{title:"Favorite users",open:o,onClose:r,favorites:[...s.filter(e=>n(e.name)).sort((e,a)=>e.name.toLowerCase().localeCompare(a.name.toLowerCase()))]})}export{v as default};
-//# sourceMappingURL=FavoriteUsersDialog-C81mo8GL.js.map
