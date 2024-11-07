bb32cc97299119a1d3fce7afa3101a30038fe099
codefeels <colindiesh@protonmail.com>
2024-11-07 16:30:55 -0500

    Updates

---
+++ assets/Link-Cncewvxs.js
@@ -0,0 +1,2 @@
+import{j as i}from"./index-FGY6sCKv.js";function u({href:e,target:t,rel:r,children:s}){return i.jsx("a",{className:"underline text-blue-600 hover:text-blue-800 visited:text-purple-600",target:t,rel:r,href:e,children:s})}export{u as L};
+//# sourceMappingURL=Link-Cncewvxs.js.map
