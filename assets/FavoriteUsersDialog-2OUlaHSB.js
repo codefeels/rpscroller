787087dfbce22ce239b554fa6818a774ce80b95b
codefeels <colindiesh@protonmail.com>
2024-11-08 09:00:00 -0500

    Updates

--- assets/FavoriteUsersDialog-2OUlaHSB.js
+++
@@ -1,2 +0,0 @@
-import{u as i,j as m,k as n}from"./index-FGY6sCKv.js";import{F as p}from"./FavoritesDialog-Dr-aWeZR.js";import"./en-US-Ckk-QizZ.js";function v({open:o,onClose:r}){const t=i(),{favorites:s}=t;return m.jsx(p,{title:"Favorite users",open:o,onClose:r,favorites:[...s.filter(e=>n(e.name)).sort((e,a)=>e.name.toLowerCase().localeCompare(a.name.toLowerCase()))]})}export{v as default};
-//# sourceMappingURL=FavoriteUsersDialog-2OUlaHSB.js.map
