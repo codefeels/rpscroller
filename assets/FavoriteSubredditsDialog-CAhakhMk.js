124f818fb074eccfa3513e51a20a0c4e73c3f04f
codefeels <colindiesh@protonmail.com>
2024-11-09 02:06:27 -0500

    Updates

---
+++ assets/FavoriteSubredditsDialog-CAhakhMk.js
@@ -0,0 +1,2 @@
+import{u as i,j as m,k as n}from"./index-ebnxPXj1.js";import{F as u}from"./FavoritesDialog-DzjBW4ln.js";import"./en-US-Ckk-QizZ.js";function l({open:t,onClose:o}){const r=i(),{favorites:s}=r;return m.jsx(u,{title:"Favorite subreddits",open:t,onClose:o,favorites:[...s.filter(e=>!n(e.name)).sort((e,a)=>e.name.toLowerCase().localeCompare(a.name.toLowerCase()))]})}export{l as default};
+//# sourceMappingURL=FavoriteSubredditsDialog-CAhakhMk.js.map
