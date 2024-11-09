232674008c9d9c36abfb2505ca25e09e68b42192
codefeels <colindiesh@protonmail.com>
2024-11-09 01:54:52 -0500

    Updates

---
+++ assets/FavoriteUsersDialog-s0-fVMoC.js
@@ -0,0 +1,2 @@
+import{u as i,j as m,k as n}from"./index-BkAPqfe_.js";import{F as p}from"./FavoritesDialog-C3HrGWpJ.js";import"./en-US-Ckk-QizZ.js";function v({open:o,onClose:r}){const t=i(),{favorites:s}=t;return m.jsx(p,{title:"Favorite users",open:o,onClose:r,favorites:[...s.filter(e=>n(e.name)).sort((e,a)=>e.name.toLowerCase().localeCompare(a.name.toLowerCase()))]})}export{v as default};
+//# sourceMappingURL=FavoriteUsersDialog-s0-fVMoC.js.map
---
+++ assets/Link-Cy7XXl9j.js
@@ -0,0 +1,2 @@
+import{j as i}from"./index-BkAPqfe_.js";function u({href:e,target:t,rel:r,children:s}){return i.jsx("a",{className:"underline text-blue-600 hover:text-blue-800 visited:text-purple-600",target:t,rel:r,href:e,children:s})}export{u as L};
+//# sourceMappingURL=Link-Cy7XXl9j.js.map
