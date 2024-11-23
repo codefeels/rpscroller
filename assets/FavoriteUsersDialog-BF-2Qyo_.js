be440f96cbd7be14408989681b21466f905a11b3
codefeels <colindiesh@protonmail.com>
2024-11-22 19:58:47 -0500

    Updates

--- assets/FavoriteUsersDialog-BF-2Qyo_.js
+++
@@ -1,2 +0,0 @@
-import{u as i,j as m,k as n}from"./index-3I-fqnGK.js";import{F as p}from"./FavoritesDialog-CmYuYVzQ.js";import"./en-US-Ckk-QizZ.js";function v({open:o,onClose:r}){const t=i(),{favorites:s}=t;return m.jsx(p,{title:"Favorite users",open:o,onClose:r,favorites:[...s.filter(e=>n(e.name)).sort((e,a)=>e.name.toLowerCase().localeCompare(a.name.toLowerCase()))]})}export{v as default};
-//# sourceMappingURL=FavoriteUsersDialog-BF-2Qyo_.js.map
