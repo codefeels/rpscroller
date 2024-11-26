009cb294fdf2eec90c821e859f7b01fbc50f60b1
codefeels <colindiesh@protonmail.com>
2024-11-26 13:49:15 -0500

    Updates

---
+++ assets/FavoriteSubredditsDialog-VR9AmifL.js
@@ -0,0 +1,2 @@
+import{u as i,j as m,l as n}from"./index-CM_eYLlH.js";import{F as u}from"./FavoritesDialog-CQUq5nwV.js";import"./en-US-Ckk-QizZ.js";function l({open:t,onClose:o}){const r=i(),{favorites:s}=r;return m.jsx(u,{title:"Favorite subreddits",open:t,onClose:o,favorites:[...s.filter(e=>!n(e.name)).sort((e,a)=>e.name.toLowerCase().localeCompare(a.name.toLowerCase()))]})}export{l as default};
+//# sourceMappingURL=FavoriteSubredditsDialog-VR9AmifL.js.map
---
+++ assets/FavoriteUsersDialog-Co1EbcNo.js
@@ -0,0 +1,2 @@
+import{u as i,j as m,l as n}from"./index-CM_eYLlH.js";import{F as p}from"./FavoritesDialog-CQUq5nwV.js";import"./en-US-Ckk-QizZ.js";function v({open:o,onClose:r}){const t=i(),{favorites:s}=t;return m.jsx(p,{title:"Favorite users",open:o,onClose:r,favorites:[...s.filter(e=>n(e.name)).sort((e,a)=>e.name.toLowerCase().localeCompare(a.name.toLowerCase()))]})}export{v as default};
+//# sourceMappingURL=FavoriteUsersDialog-Co1EbcNo.js.map
