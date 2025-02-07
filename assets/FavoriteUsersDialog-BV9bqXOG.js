da79f3205f43222bf76112ec4a79ed2c35b3bda4
codefeels <colindiesh@protonmail.com>
2025-02-07 03:53:20 -0500

    Updates

--- assets/FavoriteUsersDialog-BV9bqXOG.js
+++
@@ -1,2 +0,0 @@
-import{u as i,j as m,l as n}from"./index-BkFpsE9R.js";import{F as p}from"./FavoritesDialog-CjaRxRth.js";import"./en-US-Ckk-QizZ.js";function v({open:o,onClose:r}){const t=i(),{favorites:s}=t;return m.jsx(p,{title:"Favorite users",open:o,onClose:r,favorites:[...s.filter(e=>n(e.name)).sort((e,a)=>e.name.toLowerCase().localeCompare(a.name.toLowerCase()))]})}export{v as default};
-//# sourceMappingURL=FavoriteUsersDialog-BV9bqXOG.js.map
---
+++ assets/FavoriteUsersDialog-DK_yGJAM.js
@@ -0,0 +1,2 @@
+import{u as i,j as m,l as n}from"./index-C120Daol.js";import{F as p}from"./FavoritesDialog-BopGQuw3.js";import"./en-US-haJ8O9Oa.js";function v({open:o,onClose:r}){const t=i(),{favorites:s}=t;return m.jsx(p,{title:"Favorite users",open:o,onClose:r,favorites:[...s.filter(e=>n(e.name)).sort((e,a)=>e.name.toLowerCase().localeCompare(a.name.toLowerCase()))]})}export{v as default};
+//# sourceMappingURL=FavoriteUsersDialog-DK_yGJAM.js.map
