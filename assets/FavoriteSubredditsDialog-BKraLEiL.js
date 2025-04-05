2d137a3a486c2ff1f87bd7f4846bd26f9b41897a
codefeels <colindiesh@protonmail.com>
2025-04-05 02:44:07 -0600

    Updates

--- assets/FavoriteSubredditsDialog-BKraLEiL.js
+++
@@ -1,2 +0,0 @@
-import{u as i,j as m,l as n}from"./index-C120Daol.js";import{F as u}from"./FavoritesDialog-BopGQuw3.js";import"./en-US-haJ8O9Oa.js";function l({open:t,onClose:o}){const r=i(),{favorites:s}=r;return m.jsx(u,{title:"Favorite subreddits",open:t,onClose:o,favorites:[...s.filter(e=>!n(e.name)).sort((e,a)=>e.name.toLowerCase().localeCompare(a.name.toLowerCase()))]})}export{l as default};
-//# sourceMappingURL=FavoriteSubredditsDialog-BKraLEiL.js.map
