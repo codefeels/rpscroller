3a8421c6baa4e8fe55227c6a87da0259ca5ec889
codefeels <colindiesh@protonmail.com>
2025-05-21 00:43:32 -0400

    Wow

--- src/RedGifsCard.tsx
+++ src/RedGifsCard.tsx
@@ -2,26 +2,22 @@ import { useState } from 'react'
 
 import { useDebounceValue, useIntersectionObserver } from 'usehooks-ts'
 
-import Button from './Button'
 import { useAppStore } from './store'
 import { useSmallScreen } from './useSmallScreen'
-import { type Post, redGifUrlToId } from './util'
+import { redGifUrlToId } from './util'
 
-function RedGifsCardLoaded({
-  post,
-  superTall,
-}: {
-  superTall: boolean
-  post: Post
-}) {
+import type { Post } from './util'
+
+function RedGifsCardLoaded({ post, val }: { post: Post; val: number }) {
   const { title, url } = post
-  const h = superTall ? 'h-[200vh] w-full' : 'h-[80vh] w-full'
+  console.log({ val })
   return (
     <>
       <iframe
         title={title}
         src={`https://www.redgifs.com/ifr/${redGifUrlToId(url)}`}
-        className={`${h} overflow-hidden`}
+        className="w-full overflow-hidden"
+        style={{ height: `${val}vh` }}
         loading="lazy"
         allowFullScreen
       />
@@ -29,10 +25,11 @@ function RedGifsCardLoaded({
   )
 }
 
-function RedGifsLoading({ superTall }: { superTall: boolean }) {
+function RedGifsLoading({ val }: { val: number }) {
   return (
     <div
-      className={`animate-pulse space-y-4 w-full ${superTall ? 'h-[200vh] w-full' : 'h-[80vh] w-full'}`}
+      className={`animate-pulse space-y-4 w-full`}
+      style={{ height: `${val}vh` }}
     >
       <div className="rounded-full bg-slate-700 h-10 w-10" />
       <div className="rounded-full bg-slate-700 h-10 w-10" />
@@ -45,9 +42,9 @@ function RedGifsLoading({ superTall }: { superTall: boolean }) {
 export default function RedGifsCard({ post }: { post: Post }) {
   const { isFullscreen } = useAppStore()
 
-  const [superTall, setSuperTall] = useState(false)
+  const [val, setVal] = useState(80)
   const { isIntersecting, ref } = useIntersectionObserver({
-    threshold: isFullscreen ? 1 : superTall ? 0.2 : 0.4,
+    threshold: isFullscreen ? 1 : val > 100 ? 0.2 : 0.4,
   })
   const [debouncedIsIntersecting] = useDebounceValue(isIntersecting, 1000)
   const small = useSmallScreen()
@@ -55,18 +52,19 @@ export default function RedGifsCard({ post }: { post: Post }) {
   return (
     <div ref={ref}>
       {small ? null : (
-        <Button
-          onClick={() => {
-            setSuperTall(!superTall)
-          }}
-        >
-          {superTall ? 'Normal' : 'Supertall'}
-        </Button>
+        <input
+          type="range"
+          min={50}
+          max={300}
+          className="range [--range-fill:0]"
+          value={val}
+          onChange={event => setVal(+event.target.value)}
+        />
       )}
       {debouncedIsIntersecting ? (
-        <RedGifsCardLoaded superTall={superTall} post={post} />
+        <RedGifsCardLoaded val={val} post={post} />
       ) : (
-        <RedGifsLoading superTall={superTall} />
+        <RedGifsLoading val={val} />
       )}
     </div>
   )
