ec92c737f4f80249ad4c951f75b6679158e72b34
codefeels <colindiesh@protonmail.com>
2025-06-18 16:39:06 -0400

    Updates

---
+++ assets/FavoriteUsersDialog-pzDAwbSJ.js
@@ -0,0 +1,2 @@
+import{u as i,j as m,i as n}from"./index-CJ1xeFQK.js";import{F as p}from"./FavoritesDialog-zqcEh8ZP.js";import"./BaseDialog-C-DOVUfI.js";import"./en-US-DK8Z8mF7.js";function c({open:o,onClose:r}){const t=i(),{favorites:s}=t;return m.jsx(p,{title:"Favorite users",open:o,onClose:r,favorites:[...s.filter(e=>n(e.name)).sort((e,a)=>e.name.toLowerCase().localeCompare(a.name.toLowerCase()))]})}export{c as default};
+//# sourceMappingURL=FavoriteUsersDialog-pzDAwbSJ.js.map
---
+++ assets/MobileApp-MmZerluT.js
@@ -0,0 +1,2 @@
+import{u as t,j as s}from"./index-CJ1xeFQK.js";import{S as o,F as n}from"./Sidebar-vtNO5HpB.js";import"./BaseDialog-C-DOVUfI.js";function m(){const e=t(),{sidebarOpen:r}=e;return s.jsxs(s.Fragment,{children:[r?s.jsx(o,{}):null,s.jsx(n,{})]})}export{m as default};
+//# sourceMappingURL=MobileApp-MmZerluT.js.map
