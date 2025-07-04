a1a7219164303431164b2b1f78d38c2c7887019b
codefeels <colindiesh@protonmail.com>
2025-07-04 09:00:32 -0400

    Better arrows

--- src/GalleryCard.tsx
+++ src/GalleryCard.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-
-import Button from './Button'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 import ImageCard from './ImageCard'
 import { type Post, decode } from './util'
 
@@ -19,32 +18,37 @@ export default function GalleryCard({ post }: { post: Post }) {
         {caption} {frame + 1}/{items.length}
       </div>
 
-      <ImageCard
-        title={caption}
-        url={decode(
-          media_metadata?.[media_id]?.s.u ??
-            media_metadata?.[media_id]?.s.gif ??
-            '',
-        )}
-      />
-
-      <div className="flex justify-center">
-        <Button
+      <div className="flex items-center">
+        {/* Left navigation button */}
+        <button
+          className="p-2 flex items-center justify-center disabled:opacity-30 hover:bg-gray-200 rounded transition-colors duration-200"
           disabled={frame <= 0}
           onClick={() => {
             setFrame(frame - 1)
           }}
         >
-          &lt;
-        </Button>
-        <Button
+          <FaChevronLeft size={32} className="text-gray-600" />
+        </button>
+
+        <ImageCard
+          title={caption}
+          url={decode(
+            media_metadata?.[media_id]?.s.u ??
+              media_metadata?.[media_id]?.s.gif ??
+              '',
+          )}
+        />
+
+        {/* Right navigation button */}
+        <button
+          className="p-2 flex items-center justify-center disabled:opacity-30 hover:bg-gray-200 rounded transition-colors duration-200"
           disabled={frame >= items.length - 1}
           onClick={() => {
             setFrame(frame + 1)
           }}
         >
-          &gt;
-        </Button>
+          <FaChevronRight size={32} className="text-gray-600" />
+        </button>
       </div>
     </div>
   ) : (
